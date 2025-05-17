efe0b94a382698de547e18549661f9a341e5c6bd
fbressa <filipebr3sa@gmail.com>
2025-05-17 18:29:53 -0300

    add sidebar + seletor de tipos de arquivos

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+TIPOS_VALIDOS = ["Youtube", "Site", "pdf", "txt", "csv", "docx", "pptx"]
+
 def page_chat():
     st.header("Chat with AI")
     
@@ -24,9 +26,36 @@ def page_chat():
         chat = st.chat_message("ai")
         chat.markdown(resposta_ai)
 
+def sidebar():
+    tabs = st.tabs(["Upload de arquivos", "Seleção de modelos"])
+    with tabs[0]:
+        st.header("Upload de arquivos")
+        tipo_arquivo = st.selectbox('Selecione o tipo de arquivo', TIPOS_VALIDOS)
+        
+        if tipo_arquivo == "Youtube":
+            arquivo = st.text_input("Cole o link do vídeo do Youtube")
+        if tipo_arquivo == "Site":
+            arquivo = st.text_input("Cole o link do site")
+        if tipo_arquivo == "pdf":
+            arquivo = st.file_uploader("Selecione o arquivo PDF", type=["pdf"])
+        if tipo_arquivo == "txt":
+            arquivo = st.file_uploader("Selecione o arquivo TXT", type=["txt"])
+        if tipo_arquivo == "csv":
+            arquivo = st.file_uploader("Selecione o arquivo CSV", type=["csv"])
+        if tipo_arquivo == "docx":
+            arquivo = st.file_uploader("Selecione o arquivo DOCX", type=["docx"])
+        if tipo_arquivo == "pptx":
+            arquivo = st.file_uploader("Selecione o arquivo PPTX", type=["pptx"])
+
+
+
+    with tabs[1]:
+        st.header("Seleção de modelos")
+        
 
 def main():
     page_chat()
-
+    with st.sidebar:
+        sidebar()
 if __name__ == "__main__":
     main()
