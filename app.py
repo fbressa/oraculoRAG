68a9b3d27663a17d4c903901fe35b2761ae63844
fbressa <filipebr3sa@gmail.com>
2025-06-13 13:37:27 -0300

    feat/carrega_modelo

--- app.py
+++ app.py
@@ -1,34 +1,56 @@
 import streamlit as st
+from langchain.memory import ConversationBufferMemory
+
+from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 
 TIPOS_VALIDOS = ["Youtube", "Site", "pdf", "txt", "csv", "docx", "pptx"]
 
-CONFIG_MODELOS = {"Groq": {"modelos": ["gemma2-9b-it", "llama-3.3-70b-versatile"]},
-                  "OpenAI": {"modelos": ["gpt-3.5-turbo-0125", "o4-mini-2025-04-16", "chatgpt-4o-latest", "gpt-4.1-mini-2025-04-14"]}}
+CONFIG_MODELOS = {"Groq": 
+                        {"modelos": ["gemma2-9b-it", "llama-3.3-70b-versatile"],
+                         "chat": ChatGroq},
+                  "OpenAI": 
+                        {"modelos": ["gpt-3.5-turbo-0125", "o4-mini-2025-04-16", "chatgpt-4o-latest", "gpt-4.1-mini-2025-04-14"],
+                         "chat": ChatOpenAI}}
 
+MEMORIA = ConversationBufferMemory()
+MEMORIA.chat_memory.add_user_message("Olá IA! Você pode me ajudar com algumas perguntas?")
+MEMORIA.chat_memory.add_ai_message("Olá! Estou aqui para ajudar. O que você gostaria de saber?")
+
+def carrega_modelo(provedor, modelo, api_key):
+    chat =  CONFIG_MODELOS[provedor]["chat"](
+        model=modelo,
+        api_key=api_key
+    )
+    st.session_state["chat"] = chat
+    
 def page_chat():
     st.header("Chat with AI")
     
-    mensagens = st.session_state.get("mensagens", [])
-    if "mensagens" not in st.session_state:
-        st.session_state.mensagens = []
-    
-    for mensagem in mensagens:
-        chat = st.chat_message(mensagem[0])
-        chat.markdown(mensagem[1])
+    chat_model = st.session_state.get('chat')
+    memoria = st.session_state.get("memoria", MEMORIA)
+    if "memoria" not in st.session_state:
+        st.session_state["memoria"] = memoria
+
+    for mensagem in memoria.chat_memory.messages:
+        chat = st.chat_message(mensagem.type)
+        chat.markdown(mensagem.content)
 
     input_usuario = st.chat_input("Digite sua mensagem")
     if input_usuario:
-        mensagens.append(("user", input_usuario))
-        st.session_state['mensagens'] = mensagens
-        chat = st.chat_message("user")
+        memoria.chat_memory.add_user_message(input_usuario)
+        chat = st.chat_message('human')
         chat.markdown(input_usuario)
-        
-        # Simulate AI response
-        resposta_ai = "Esta é uma resposta simulada da IA."
-        st.session_state.mensagens.append(("ai", resposta_ai))
-        chat = st.chat_message("ai")
-        chat.markdown(resposta_ai)
 
+        chat = st.chat_message('ai')
+        chat.write_stream(chat_model.stream(input_usuario))
+        
+        resposta = chat_model.invoke(input_usuario).content
+        memoria.chat_memory.add_ai_message(resposta)
+
+        st.session_state["memoria"] = memoria
+        st.rerun()
+        
 def sidebar():
     tabs = st.tabs(["Upload de arquivos", "Seleção de modelos"])
     with tabs[0]:
@@ -60,6 +82,9 @@ def sidebar():
             st.session_state[f'api_key_{provedor}'] = api_key
             st.success(f"Chave de API para {provedor} salva com sucesso!")
         st.info("As chaves de API são armazenadas em sessão e não são salvas permanentemente.")
+
+    if st.button("Carregar modelo", use_container_width=True):
+        carrega_modelo(provedor, modelo, api_key)
 
 def main():
     page_chat()
